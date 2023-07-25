fadfb22662ce85d426dac05536b10ef40b6d0fe3
thomas murat <thomas.murat974@gmail.com>
2023-07-25 18:53:46 +0200

    seed article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+require 'faker'
+
+# Supprimez tous les articles existants pour éviter les doublons lors de l'exécution multiple des seeds
+Article.delete_all
+
+# Générez 10 faux articles
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n")
+  )
+end
+
+puts '10 faux articles ont été générés dans la base de données.'
